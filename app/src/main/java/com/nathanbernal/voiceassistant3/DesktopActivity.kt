6d80613838f850a7ee8c39f464f68bfc9b0fb71c
Nathan Bernal <jo.bernalv@duocuc.cl>
2025-01-27 23:42:23 -0300

    Desarrollo

--- app/src/main/java/com/nathanbernal/voiceassistant3/DesktopActivity.kt
+++ app/src/main/java/com/nathanbernal/voiceassistant3/DesktopActivity.kt
@@ -1,14 +1,31 @@
 package com.nathanbernal.voiceassistant3
 
+import android.app.Activity
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class DesktopActivity : AppCompatActivity() {
+class DesktopActivity : ComponentActivity() {
+
+    private var btnItems = emptyArray<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_desktop)
+
+        val btnHola = findViewById<Button>(R.id.btnHola)
+        btnHola.setOnClickListener {
+            val mp = MediaPlayer.create(this, R.raw.micontactoes)
+        }
+
     }
+
+
+
 }
